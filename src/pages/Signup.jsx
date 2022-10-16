38e1f726a25ab0d484930f8a351d9e583e19e4d2
Neluka <nelukarulzz@gmail.com>
2022-10-16 14:26:51 +0530

    edited sign up

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -29,13 +29,13 @@ export default function Signup() {
   <div className="w-full md:w-[67%] ml-4 mr-4 lg:w-[40%] lg:mt-10" >
   <form >
     <div>
-    <input className="border-2 transition duration-500 placeholder-blue-200 focus:placeholder-transparent border-blue-400 w-full py-2  text-black-400 bg-transparent rounded-md focus:outline-none text-left md:p mb-4" type="text" id='name' value={name}onChange={onChange} placeholder="name"/>
+    <input className="border-2 transition duration-500 placeholder-gray-400 focus:placeholder-transparent border-blue-400 w-full py-2  text-black-400 bg-transparent rounded-md focus:outline-none text-left md:p mb-4" type="text" id='name' value={name}onChange={onChange} placeholder="name"/>
     </div>
     <div>
-    <input className="border-2 transition duration-500 placeholder-blue-200 focus:placeholder-transparent border-blue-400 w-full py-2  text-black-400 bg-transparent rounded-md focus:outline-none text-left md:p" type="email" id='email' value={email}onChange={onChange} placeholder="email address"/>
+    <input className="border-2 transition duration-500 placeholder-gray-400 focus:placeholder-transparent border-blue-400 w-full py-2  text-black-400 bg-transparent rounded-md focus:outline-none text-left md:p" type="email" id='email' value={email}onChange={onChange} placeholder="email address"/>
     </div>
     <div className= "relative mb-5">
-       <input className="mt-4 border-2 transition duration-500 placeholder-blue-200 focus:placeholder-transparent border-blue-400 w-full py-2  text-black-400 bg-transparent rounded-md focus:outline-none text-left md:p" type={showPassword ? "text": "password"} id='password' value={password}onChange={onChange} placeholder="Password"/>
+       <input className="mt-4 border-2 transition duration-500 placeholder-gray-400 focus:placeholder-transparent border-blue-400 w-full py-2  text-black-400 bg-transparent rounded-md focus:outline-none text-left md:p" type={showPassword ? "text": "password"} id='password' value={password}onChange={onChange} placeholder="Password"/>
       {  showPassword? 
       <AiFillEyeInvisible className= "absolute right-3 top-7 cursor-pointer" onClick={()=>setShowpassword((prevState)=>!prevState)} /> : 
       <AiFillEye className="absolute right-3 top-7 cursor-pointer" onClick={()=>setShowpassword((prevState)=>!prevState)} /> }
@@ -47,7 +47,7 @@ export default function Signup() {
       
     </div>
     <button className="w-full bg-blue-600 text-white px-7 py-3 rounded-lg shadow-md
-  hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg" type="submit">Sign in</button>
+  hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg" type="submit">Sign up</button>
   <div className="flex items-center my-4 before:border-t before:flex-1
   before:border-gray-400 after:border-t after:flex-1 after:border-gray-400">
     <p className="text-center font-semibold mx-2 ">
