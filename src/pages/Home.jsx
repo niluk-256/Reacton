ebbb00a055926864425cec3192c72ff1e333f2cc
Neluka <nelukarulzz@gmail.com>
2022-10-24 20:12:35 +0530

    metamask

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,38 @@
 import React from "react"
 import shareVideo from "../assets/logo.mp4"
 import metaVideo from "../assets/metamask-icon.svg"
+import { abi, contractAddress } from "../constants"
+// import Meta from "../metaconnect"
 export default function Home() {
+  async function Meta() {
+    if (typeof window.ethereum !== "undifined") {
+      await window.ethereum.request({
+        method: "eth_requestAccounts",
+      })
+      console.log("Connected")
+      // console.log(ethers)
+      // inputfnd.disabled = false
+      // inputfnd.placeholder = "Amount.."
+      // icodiv[0].style.color = "green"
+      // inputfnd.style.border = "2px solid green"
+    } else {
+      console.log("there's no metamask")
+    }
+  }
+  //balance
+
   return (
     <div>
       <div>
         <div className="ml-10 mr-11 mt-4 flex justify-between text-3xl mb-5 border-gray-500 font-semibold ">
           Home
-          <button className="flex bg-slate-500 rounded-lg p-1 text-2xl font-sans text-gray-50 hover:bg-slate-900">
+          <button
+            onClick={Meta}
+            id="meta"
+            className={
+              "flex bg-slate-500 rounded-lg p-1 text-2xl font-sans text-red-500 hover:bg-slate-900 pr-2"
+            }
+          >
             <img className="w-10 mr-1" src={metaVideo} alt="" /> Connect
           </button>
         </div>
