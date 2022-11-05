14ade94b1457e717e7e988d436335dd69674505e
Neluka <nelukarulzz@gmail.com>
2022-11-05 13:51:06 +0530

    caa

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,10 +3,8 @@ import React from "react"
 export default function About() {
   return (
     <div>
-      <div className="flex justify-center text-3xl">About</div>
-      <div className="mt-12 flex justify-center text-1xl">
-        Athule wada htto{"😎 "}
-      </div>
+      <div className="flex justify-center text-3xl">About Me</div>
+      <div className="mt-12 flex justify-center text-1xl">Hey</div>
     </div>
   )
 }
